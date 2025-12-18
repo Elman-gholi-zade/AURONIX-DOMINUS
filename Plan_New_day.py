2666f0d896ef985c0fd2be823a82d9f69e1e44be
Elman_gholi_zade <zr1390_codeQgmail.com>
2025-12-18 22:31:07 +0330

    c1 - complete __Add_New_Plan__ section

--- Plan_New_day.py
+++ Plan_New_day.py
@@ -4,7 +4,7 @@ import json
 
 
 
-class New_Day :
+class Plan :
 
 
     def __init__(self, date, row, activity, start_time, end_time, description):
@@ -14,7 +14,6 @@ class New_Day :
         self.__start_time = start_time
         self.__end_time = end_time
         self.__description = description
-
 
 
 
@@ -41,11 +40,11 @@ class New_Day :
 
 
     # Check day date
-    def check_date(self, databace) :
-        '''این تابع بررسی می کند که تاریخ 
-        از قبل داخل دیتابیس وجود نداشته باشد'''
+    def check_date(self, databace, condition) :
+        '''این تابع یا شرط ورودی دارد و آن را بررسی کرده 
+        و نتیجه را بر می گرداند'''
 
-        if self.__date not in databace :
+        if condition :
             return True
         
         else :
@@ -53,18 +52,46 @@ class New_Day :
         
 
 
+
     
+    # Check date row
+    def check_date_row(self, databace, date, row) :
+        '''این تابع بررسی می کند 
+        که ردیف ورودی در تاریخ ورودی در دیتابیس ورودی
+        نباشد'''
+        
+        try :
+            if row in databace[date][row] :
+                return False
+
+        
+        except KeyError :
+            return True
+        
+        else :
+            return False
+        
+
+            
 
 
     # Save plan in databace
-    def save_plan(self, databace) :
-        '''این تابع ابتدا ورودی های شیء را
+    def save_plan(self, databace, create_date : bool) :
+        ''' این تابع ابتدا اینکه تاریخ ساخته شود و یا خیر
+        را از ورودی می گیرد
+        سپس ورودی های شیء را
         ساختاردهی کرده و سپس
         آن را در دیتابیس برنامه ریزی روزانه 
         ذخیره می کند'''
+        
 
-        databace[self.__date] = {}
+        # ساخت تاریخ
+        if create_date :
+            databace[self.__date] = {}
 
+
+        
+        # ساختاردهی
         databace[self.__date][self.__row] = {
             "Activity" : self.__activity,
             "Start Time" : self.__start_time,
@@ -74,7 +101,7 @@ class New_Day :
         
 
         
-
+        # بازکردن و ذخیره در دیتابیس
         try :
             with open("Daily_Plan_Databace.json", "w", encoding="utf-8") as file :
                 json.dump(databace, file, ensure_ascii=False, indent=4)
@@ -90,15 +117,84 @@ class New_Day :
 
 
 
-# Create Object
-plan = New_Day("1404/09/27", 1, "Python", "11:30", "14:00", "Study lesson one of season 3 - OOP")
 
 
-databace = plan.load_databace()
-
-if plan.check_date(databace) :
-    plan.save_plan(databace)
 
 
-else :
-    print("Date arlead excastid ❌")
+
+
+
+
+
+
+
+# Start new day
+def new_day(Object, databace, date) :
+    if Object.check_date(databace, date not in databace) :
+        Object.save_plan(databace, True)
+
+
+    else :
+        print("Date arleady excastid ❌")
+
+
+
+
+
+# Add New plan on date
+def add_new_plan(Object, databace, date, row) :
+    if Object.check_date(databace, date in databace) :
+        print("OK")
+        
+        if Object.check_date_row(databace, date, row) :
+            Object.save_plan(databace, False)
+
+        else :
+            print("This row used early in this date ❌")
+
+    else :
+        print("Date not found !")
+
+
+
+
+
+
+
+def mine_meno() :
+
+    date = input("Date :  ")
+    row = input("Row :  ")
+    activity = input("Activity :  ")
+    start_time = input("Start Time :  ")
+    end_time = input("End time :  ")
+    description = input("Description :  ")
+
+
+    # Create Object
+    plan = Plan(date, row, activity, start_time, end_time, description)
+
+    databace = plan.load_databace()
+
+
+
+
+    print("1. New Day")
+    print("2. Add New Plan ")
+
+    meno = input(" >>>  ")
+
+    if meno == "1" :
+        new_day(plan, databace, date)
+
+    elif meno == "2" :
+        add_new_plan(plan, databace, date, row)
+
+
+
+
+
+
+
+
+mine_meno()
